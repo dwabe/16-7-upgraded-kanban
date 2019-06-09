d3354dddceaf4ecd216edc2d25ca3331ba356074
Bartek Błęcki <bartek@blecki.pl>
2019-06-09 21:55:59 +0200

    Update scripts

---
+++ js/App.js
@@ -0,0 +1,45 @@
+// Communication with the server
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '4103',
+    'X-Auth-Token': '6952109ee8b8518cc5e08f41eca912c2'
+};
+
+// Template generation
+function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+
+    return element;
+}
+
+// Polling array from server
+fetch(baseUrl + '/board', {
+        headers: myHeaders
+    })
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (resp) {
+        setupColumns(resp.columns);
+    });
+
+// Create new column and add to array
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+// Add new cards to column
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name);
+        col.addCard(cardObj);
+    });
+}
